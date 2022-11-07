83f2e73852e1e4ce10cf131fb2d46d066031ea28
raviraj15 <ravirajmandlik.rm@gmail.com>
2022-11-07 10:49:53 +0530

    new project created

---
+++ src/main/java/com/BikkadIT/Team01ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team01ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team01ProjectApplication.class, args);
+	}
+
+}
